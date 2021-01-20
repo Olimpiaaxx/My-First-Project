01869c480749c954a7cf94086f48220ea28b22ce
Olimpiaaxx <olimpiapater@gmail.com>
2021-01-20 21:33:02 +0000

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("Hello ");
+        Solution sl = new Solution();
+
+        System.out.print(sl.getName());
+
+    }
+}
